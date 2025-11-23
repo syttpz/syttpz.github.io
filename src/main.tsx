6266891d658bd6b93bd70fbc80747572b5e4bcab
Eddy Hu <83127670+syttpz@users.noreply.github.com>
2025-11-23 11:06:13 +0400

    BrowserRouter to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
